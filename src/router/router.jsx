982baa03644aa6660e1c68b349e42e2fe4e7476b
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-06-04 07:30:38 +0600

    LayOut dane

---
+++ src/router/router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+import Home from "../Home/Home";
+
+export const react = createBrowserRouter([
+    {
+        path:'/',Component:Root,
+        children:[
+            {index:true,Component:Home}
+        ]
+    }
+])
