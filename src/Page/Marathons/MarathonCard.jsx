cb0b54e9f8fc9de46148e4be63ff448cbf7df70b
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-06-09 10:39:43 +0600

    Marathon Card added all data done

--- src/Page/Marathons/MarathonCard.jsx
+++ src/Page/Marathons/MarathonCard.jsx
@@ -1,25 +1,54 @@
 import React from "react";
+import { GoClock } from "react-icons/go";
+import { IoLocationOutline } from "react-icons/io5";
 
 const MarathonCard = ({ marathon }) => {
+  const { photo, name, Location, StartRegistrationDate, EndRegistrationDate } =
+    marathon || {};
+
   console.log(marathon);
+
+  //   date convert
+  const StartRegistrationDateConvert = new Date(
+    StartRegistrationDate
+  ).toLocaleDateString();
+  const EndRegistrationDateConvert = new Date(
+    EndRegistrationDate
+  ).toLocaleDateString();
 
   return (
     <div>
       <div className="card bg-base-100  shadow-sm">
         <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
+          <img src={photo} alt="marathonPhoto" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
+          <h2 className="card-title">{name}</h2>
+          <p className="flex text-base text-gray-600">
+            Location:
+            <IoLocationOutline className="mt-1 ml-2 text-black font-bold" />
+            <span className="text-black font-bold">{Location}</span>
           </p>
+          <div className="space-y-2">
+            <p className="flex text-base text-gray-600">
+              StartRegistration:
+              <GoClock className="mt-1 ml-2 mr-1 text-black font-bold" />
+              <span className="text-black font-bold">
+                {StartRegistrationDateConvert}
+              </span>
+            </p>
+            <p className="flex text-base text-gray-600">
+              EndRegistration:
+              <GoClock className="mt-1 ml-2 mr-1 text-black font-bold" />
+              <span className="text-black font-bold">
+                {EndRegistrationDateConvert}
+              </span>
+            </p>
+          </div>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn bg-blue-400 text-white mt-5">
+              See Details
+            </button>
           </div>
         </div>
       </div>
