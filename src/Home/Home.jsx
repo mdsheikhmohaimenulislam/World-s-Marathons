f9da5e338b329737f64ff3353e064d65db7715ed
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-06-13 23:26:07 +0600

    Animation content done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,22 +1,55 @@
-import React from 'react';
-import CarouselSection from '../Components/CarouselSection/CarouselSection';
-import MarathonSection from '../Page/MarathonsSection/MarathonSection';
-import TicketList from '../Components/TicketList/TicketList';
-import CustomerBenefits from '../Components/TrainerSection/TrainerSection';
-import TrainerSection from '../Components/TrainerSection/TrainerSection';
-import UpcomingEvents from '../Components/UpcomingEvents /UpcomingEvents ';
-
+import React from "react";
+import CarouselSection from "../Components/CarouselSection/CarouselSection";
+import MarathonSection from "../Page/MarathonsSection/MarathonSection";
+import TicketList from "../Components/TicketList/TicketList";
+import CustomerBenefits from "../Components/TrainerSection/TrainerSection";
+import TrainerSection from "../Components/TrainerSection/TrainerSection";
+import UpcomingEvents from "../Components/UpcomingEvents /UpcomingEvents ";
+import AnimatedContent from "../Animated/AnimatedContent ";
 
 const Home = () => {
-    return (
-        <div>
-            <CarouselSection/>
-            <MarathonSection/>
-            <UpcomingEvents/>
-            <TrainerSection/>
-            <TicketList/>
-        </div>
-    );
+  return (
+    <div>
+      <CarouselSection />
+
+      {/* AnimatedContent  */}
+
+      <AnimatedContent
+        distance={300}
+        direction="horizontal"
+        initialOpacity={0}
+        duration={8}
+      >
+        <MarathonSection />
+      </AnimatedContent>
+      <AnimatedContent
+        distance={300}
+        direction="horizontal"
+        initialOpacity={0}
+        duration={11}
+      >
+        <UpcomingEvents />
+      </AnimatedContent>
+
+      <AnimatedContent
+        distance={300}
+        direction="horizontal"
+        initialOpacity={0}
+        duration={14}
+      >
+        <TrainerSection />
+      </AnimatedContent>
+
+      <AnimatedContent
+        distance={300}
+        direction="horizontal"
+        initialOpacity={0}
+        duration={16}
+      >
+        <TicketList />
+      </AnimatedContent>
+    </div>
+  );
 };
 
 export default Home;
